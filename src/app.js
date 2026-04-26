b472a2a18d5f44c37239f999bee77d0b11ebfc6a
Chethiya Kusal <chethiyakdis@gmail.com>
2026-04-26 08:50:45 +0530

    Final professional structure and fixes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const kb = require('./knowledgeBase');
-const { handleChat } = require('./chatController');
+const kb = require('./services/knowledgeService');
+const { handleChat } = require('./controllers/chatController');
 require('dotenv').config();
 
 const app = express();
